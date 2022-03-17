dc05022b0dd3a0f35de4df1e768b8d5df9cdcda7
kashanshah <kashanshah@hotmail.com>
2022-03-17 18:26:37 +0400

    Revert "feat: added google analytics script"
    This reverts commit da8b505a5a3f32aebbe13f644be93919dc860bba.

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,6 @@ import 'normalize.css';
 import { ChakraProvider } from '@chakra-ui/react';
 import { Provider } from 'react-redux';
 import { store } from '../src/redux/store';
-import Script from 'next/script';
 
 Router.events.on('routeChangeComplete', () => {
   // alert('complete');
@@ -13,29 +12,15 @@ Router.events.on('routeChangeStart', () => {
   // alert('start');
 });
 
-const ScriptTags = () => {
-  return (
-    <Script
-      id='google-analytics'
-      dangerouslySetInnerHTML={{
-        __html: `<!-- Global site tag (gtag.js) - Google Analytics --> <script async src="https://www.googletagmanager.com/gtag/js?id=UA-117582605-4"></script> <script> window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'UA-117582605-4'); </script>`,
-      }}
-    />
-  );
-};
-
 function MyApp({ Component, pageProps }) {
   return (
-    <>
-      <ScriptTags />
-      <Provider store={store}>
-        <ChakraProvider>
-          <div className='asdasd'>
-            <Component {...pageProps} />
-          </div>
-        </ChakraProvider>
-      </Provider>
-    </>
+    <Provider store={store}>
+      <ChakraProvider>
+        <div className='asdasd'>
+          <Component {...pageProps} />
+        </div>
+      </ChakraProvider>
+    </Provider>
   );
 }
 
