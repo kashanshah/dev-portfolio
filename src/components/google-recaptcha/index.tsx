bd43cae97eba015cbc7d80c7ae8a6e8633ddec54
Kashan Shah <kashan.shah@americaneagle.com>
2023-04-23 19:45:19 +0400

    feat: passing lang in recaptcha

--- src/components/google-recaptcha/index.tsx
+++ src/components/google-recaptcha/index.tsx
@@ -2,6 +2,7 @@ import ReCAPTCHA from 'react-google-recaptcha';
 import { constants } from '@utils/constants';
 import { useColorMode } from '@chakra-ui/react';
 import { RefObject } from 'react';
+import { useRouter } from 'next/router';
 
 type GoogleRecaptchaProps = {
   siteKey: string;
@@ -11,10 +12,11 @@ type GoogleRecaptchaProps = {
 export const GoogleRecaptcha = (props: GoogleRecaptchaProps) => {
   const { siteKey, onChange, reference } = props;
   const { colorMode } = useColorMode();
+  const { locale } = useRouter();
 
   if (!constants?.recaptcha?.siteKey) {
     return null;
   }
 
-  return <ReCAPTCHA sitekey={siteKey} theme={colorMode} onChange={onChange} ref={reference} />;
+  return <ReCAPTCHA lang={locale} sitekey={siteKey} theme={colorMode} onChange={onChange} ref={reference} />;
 };
