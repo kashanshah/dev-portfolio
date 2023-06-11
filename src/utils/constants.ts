6f7b4fa77874c4ea4286b57f845f1a1ab0dfb0e2
Kashan Shah <kashan.shah@americaneagle.com>
2023-06-11 21:42:17 +0400

    feat: resume-syed-kashan-ali-shah.pdf update

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -29,7 +29,7 @@ export const constants: ConstantsType = {
       ar: 'عرض سيرتي الذاتية',
       ur: 'میرا ریزیومے دیکھیں',
     },
-    link: { en: '/docs/resume-syed-kashan-ali-shah.pdf' },
+    link: { en: '/docs/resume-syed-kashan-ali-shah.pdf?v=1' },
   },
   apiBaseUrl: { en: 'https://msenterprises.pk/apis' },
   isReduxPersistEnabled: false,
